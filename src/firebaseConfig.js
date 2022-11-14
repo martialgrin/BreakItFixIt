463be326a9a521a6b688acfff9a760c637afd2af
martialgrin <martial.grin@gmail.com>
2022-11-14 16:31:00 +0100

    test for span

---
+++ src/firebaseConfig.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, update } from "firebase/database"; // TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const config = {
+	apiKey: "AIzaSyAhSjoeCqJoBWrulH9XkD4iN-hmVWccxIo",
+	authDomain: "breakitfixit-9f67d.firebaseapp.com",
+	databaseURL:
+		"https://breakitfixit-9f67d-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "breakitfixit-9f67d",
+	storageBucket: "breakitfixit-9f67d.appspot.com",
+	messagingSenderId: "206776885912",
+	appId: "1:206776885912:web:a71fe30fd7056d9be066ed",
+};
+
+export const firebaseConfig = () => {
+	const app = initializeApp(config);
+	console.log(app);
+
+	const initValues = async () => {
+		const db = getDatabase(app);
+		for (let i = 1; i < 16; i++) {
+			set(ref(db, "poster" + i), {
+				x: 0,
+				y: 0,
+				state: false,
+			});
+		}
+	};
+
+	const setValues = async (val, name) => {
+		const db = getDatabase(app);
+		update(ref(db, name), {
+			x: val.x,
+			y: val.y,
+			isTouching: val.state,
+		});
+	};
+	return { initValues, setValues };
+};
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,20 @@
 <script>
 	("use strict");
-	import { setPointerControls, getCenterOfTwoPoints } from "svelte-gestures";
+	import {
+		setPointerControls,
+		getCenterOfTwoPoints,
+		pan,
+	} from "svelte-gestures";
+	import { firebaseConfig } from "../../firebaseConfig";
+	import { page } from "$app/stores";
+	let isPage = false;
+	if ($page.url.pathname != "/") {
+		isPage = true;
+	}
+
+	const firebase = firebaseConfig();
+
+	firebase.initValues();
 
 	export function multiTouch(node, parameters = { touchCount: 1 }) {
 		const gestureName = "multiTouch";
@@ -50,20 +64,32 @@
 
 	function handler(event) {
 		scale = event.detail.scale;
-		x = event.detail.center.x;
-		y = event.detail.center.y;
+		x = event.detail.x;
+		y = event.detail.y;
+		firebase.setValues(
+			{ x: x, y: y, state: true },
+			"poster" + $page.url.pathname.slice(1)
+		);
 	}
 	// create a manager for that element
 </script>
 
 <!-- <p>Current URL: {$page.url.pathname}</p> -->
 <div
+	use:pan={{ delay: 300 }}
+	on:pan={handler}
 	use:multiTouch={{ touchCount: 2 }}
 	on:multiTouch={handler}
-	style="width:500px;height:500px;border:1px solid black;"
+	id="stage"
 >
 	multi touch center: x {x}, y {y}
 </div>
 
 <style>
+	#stage {
+		box-sizing: border-box;
+		border: 1px solid black;
+		width: 100vw;
+		height: calc(100vw * 1.77);
+	}
 </style>
